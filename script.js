9877808c0ef67a9e3422a42f6e8e3343c74d712f
gonzalote99 <gonzalodavila759@gmail.com>
2022-07-05 14:45:51 -0300

    2 files modified, 1 added, 2 deleted

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Hello!');
+function showTime() {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "AM";
+
+  if (h == 0) {
+      h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? `0${h}`: h;
+  h = m < 10 ?  `0${h}`: h;
+  h = s < 10? `0${h}`: h;
+
+  let time = h + ":" + m + ":" + s + " " + session;
+  document.querySelector('.clock').innerText = time;
+  document.querySelector(".clock").innerHTML = time;
+  
+  setTimeout(showTime, 1000);
+
+}
+
+showTime();
